c5baf9c769dbdd90a5d598fc91453789f027a250
HongLeongYong <32185642+HongLeongYong@users.noreply.github.com>
2021-03-16 20:51:28 +0800

    update 1.0
    create longestCommonPrefix.java
    
    https://leetcode.com/problems/longest-common-prefix/

---
+++ longestCommonPrefix.java
@@ -0,0 +1,40 @@
+//LeetCode 網址： https://leetcode.com/problems/longest-common-prefix/
+
+package prefix;
+
+/**
+ *
+ * @author HONG LEONG
+ */
+public class Prefix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[ ] testString = {"flower","flow","flozen"};
+        Prefix p1 = new Prefix();
+        System.out.println("return = " + p1.longestCommonPrefix(testString));        
+    }
+    
+    public String longestCommonPrefix(String[ ] strs){
+//        System.out.println("strs[0] = " + strs[0]);
+//        System.out.println("strs.length = " + strs.length);
+//        System.out.println("strs[0].length() = " + strs[0].length());
+        
+// 當沒有東西的時候就回傳空值
+        if(strs == null || strs.length == 0 ) return "";
+ // 預設Prefix 為第一個字串
+        String prefix = strs[0];
+
+        for(int x = 1 ; x < strs.length ; x ++){
+  // int indexOf(String str, int fromIndex): 返回从 fromIndex 位置开始查找指定字符在字符串中第一次出现处的索引，如果此字符串中没有这样的字符，则返回 -1。;
+            while(strs[x].indexOf(prefix) != 0){
+                // prefix 逐漸縮短，用於比對
+                prefix = prefix.substring(0, prefix.length()-1);                
+                if (prefix.isEmpty()) return "";
+            }          
+        }        
+     return prefix;
+    }    
+}
